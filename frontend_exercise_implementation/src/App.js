aac65b88915d711a5790dc7ac371e490f411a13f
Eleanor Fuller <Eleanor.Fuller@red-gate.com>
2019-10-25 17:38:49 +0100

    Output pet type and amount

--- frontend_exercise_implementation/src/App.js
+++ frontend_exercise_implementation/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React , { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const totalData = data.pets
+    console.log(totalData.data)
+    const petsData = totalData.find(data => data.title == 'Pets in Cambridge').data
+    //const petsData = totalData.map(x => x.data)
+    
+    
+   /* var petsData = {
+      "cat": 55,
+      "dog": 14,
+      "lizard": 37,
+      "elephant": 3
+    }*/
+    console.log(petsData)
+    
+    const petList = Object.keys(petsData).map(key => 
+        <li key={key} value={key}> {petsData[key]} : {key}</li>
+    )
+
+    const dataUI = totalData.map((data) => 
+    <div>
+      <h1>{data.title}</h1>
+      {petList}
+    </div>)
+
+    return (
+      <div>
+        <div>Hello World</div>
+        <div className="App">
+          <p>{dataUI}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+class ObjectJSON extends Component {
+  
 }
 
 export default App;
---
+++ frontend_exercise_implementation/src/data.js
@@ -0,0 +1,11 @@
+export default {
+    pets: [{
+        "title": 'Pets in Cambridge',
+        "data": {
+          "cat": 55,
+          "dog": 14,
+          "lizard": 37, // There are a lot of Liz lovers in Cambridge. :D
+          "elephant": 3
+        }
+      }]
+}
